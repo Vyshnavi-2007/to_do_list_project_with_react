3d42b9f18f20aa8d09c99c828f7cbb1a7545b091
Sri Shanmukha Vyshnavi Priya Gottipati <priya.gottipati24@sasi.ac.in>
2026-07-26 15:13:35 +0530

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -58,7 +58,7 @@ function Login() {
               className="eye-btn"
               onClick={() => setShowPassword(!showPassword)}
             >
-              {showPassword ? "🙈" : "👁"}
+              {showPassword ? "" : "👁"}
             </button>
           </div>
 
